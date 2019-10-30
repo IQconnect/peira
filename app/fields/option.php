7474a2fe293b0fba9be02218669b8e81a2a6f31b
Robert <robert.m93a@gmail.com>
2019-10-30 14:19:51 +0100

    footer

--- app/fields/option.php
+++ app/fields/option.php
@@ -14,5 +14,17 @@ $option
         ->addImage('logo_header', ['label'=>'Logo Header'])
     ->addTab('contact', ['label'=>'Informacje kontatkowe', 'placement' => 'left'])
         ->addText('phone', ['label'=>'Telefon'])
+    ->addTab('footer', ['label' => 'Stopka', 'placement' => 'left'])
+    ->addGroup('footer_info')
+        ->addImage('image', ['label'=>'Zdjęcie w tle'])
+        ->addText('email', ['label' => 'Email'] )
+        ->addText('firm', ['label' => 'Firma'])
+        ->addText('adress', ['label' => 'Adres'] )
+        ->addText('krs', ['label' => 'KRS'] )
+        ->addText('nip', ['label' => 'NIP'] )
+        ->addText('regon', ['label' => 'Regon'] )
+    ->endGroup()
+    ->addTab('Ilikeart', ['label' => 'Ilikeart', 'placement' => 'left'])
+    ->addImage('Ilikeart_logo', ['label'=>'Zdjęcie w tle'])
     ;
 return $option;
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,59 @@
+@php
+  $phone = option('phone');
+  $footer = option('footer_info');
+  $logo = $footer['image'];
+  $iq = option('Ilikeart_logo');
+@endphp
+
+<footer class="footer">
+	{!! image($footerImg['ID'], 'full', 'footer__bg') !!}
+	<div class="container">
+			@if($logo)
+			<img
+				src="{{ $logo['url'] }}"
+				alt="Peira"
+				class="footer__logo"
+			/>
+		  @endif
+	  <div class="footer__content">
+
+		<div class="footer__left">
+
+			<ul class="footer__info link--menu">
+				<li> {{$footer['firm'] }} </li>
+				<li> {{$footer['adress'] }} </li>
+				<li> KRS: {{$footer['krs'] }} NIP: {{$footer['nip'] }} </li>
+				<li> REGON: {{$footer['regon'] }} </li>
+			</ul>
+		</div>
+		<nav class="footer__nav">
+			@if (has_nav_menu('footer_navigation'))
+				{!! wp_nav_menu(['theme_location' => 'footer_navigation', 'menu_class' => 'footer__nav']) !!}
+			@endif
+		</nav>
+		<nav class="footer__secondnav" >
+				@if (has_nav_menu('footer_second'))
+					{!! wp_nav_menu(['theme_location' => 'footer_second','menu_class' => 'footer__nav']) !!}
+				@endif
+			</nav>
+			<div class="footer__button">
+			@if ($phone)
+			<a href="tel:{{ clearSpace($phone) }}" class=" button button--icon">
+				@include('svg.phone')
+				{{ $phone }}
+			</a>
+			@endif
+			<a href="#" class="footer__email text">{{$footer['email'] }}</a>
+			</div>
+	  </div>
+	<div class="footer__copyright link--menu">
+		COPYRIGHT 2019 © PEIRA.PL. WSZELKIE PRAWA ZASTRZEŻONE.
+		@if($iq)
+		<a class="footer__madeby"href="https://ilike-art.com/">
+		 	created by <img class="footer__by" src="{!! $iq['url'] !!}" alt="logo" />
+		</a>
+		@endif
+	</div>
+	  </div>
+	</div>
+  </footer>
