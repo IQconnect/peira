45e320782ae8f873cd8006b875578fd18471a93a
Kamil <kmirosz6@gmail.com>
2019-11-06 09:10:55 +0100

    invest slider

--- resources/assets/scripts/components/invest-slider.js
+++ resources/assets/scripts/components/invest-slider.js
@@ -1,54 +1,67 @@
-var Flickity = require('flickity');
+var Flickity = require('flickity-as-nav-for');
 import 'flickity-fade';
-import 'flickity-as-nav-for';
 
 const CONFIG = {
     ELEM: '.invest-slider__carousel',
     CELL: '.invest-slider__cell',
     NAV: '.invest-slider__nav-carousel',
     NAV_CELL: '.invest-slider__nav-cell',
+    SITCHER: '[data-invest-slider-index]',
+    WRAPPER: '[data-invest-slider]',
+    CLASS: '-is-active',
 };
 
 const InvestSlider = {
     init() {
-        const { ELEM, CELL, NAV, NAV_CELL } = CONFIG;
+        const { ELEM, NAV, SITCHER, WRAPPER, CLASS} = CONFIG;
         this.elem = document.querySelectorAll(ELEM);
         this.nav = document.querySelectorAll(NAV);
+        this.switcher = document.querySelectorAll(SITCHER);
+        this.wrapper = document.querySelector(WRAPPER);
+        
+        this.class = CLASS;
 
         this.sliderArray = [];
-        this.navArray= [];
+        this.navArray = [];
 
-        console.log('Init invest-slider', this.elem)
+        console.log('Init invest-slider', this.wrapper)
 
         if (this.elem) {
-            this.elem.forEach((element, index) => {
-                this.slider = new Flickity(element, {
-                    pageDots: false,
-                    prevNextButtons: true,
-                    cellSelector: CELL,
-                    wrapAround: true,
-                    fade: false,
-                });
-
-                this.sliderNav = new Flickity(this.nav[index], {
-                    pageDots: false,
-                    prevNextButtons: true,
-                    cellSelector: NAV_CELL,
-                    wrapAround: true,
-                    fade: false,
-                    asNavFor: ELEM,
-                });
-
-                console.log('index', index)
-
-                this.sliderArray.push(this.slider);
-                this.navArray.push(this.sliderNav);
-
-                setTimeout(() => {
-                    this.resize();
-                }, 2000)
-            });
+            this.slider();
+            this.addEvent();
         }
+    },
+
+    slider() {
+        const { NAV_CELL, CELL} = CONFIG;
+
+        this.elem.forEach((element, index) => {
+            this.slider = new Flickity(element, {
+                pageDots: false,
+                prevNextButtons: true,
+                cellSelector: CELL,
+                wrapAround: true,
+                fade: false,
+            });
+
+            this.sliderNav = new Flickity(this.nav[index], {
+                pageDots: false,
+                prevNextButtons: true,
+                cellSelector: NAV_CELL,
+                wrapAround: true,
+                fade: false,
+                asNavFor: element,
+            });
+
+            console.log('index', index)
+
+            this.sliderArray.push(this.slider);
+            this.navArray.push(this.sliderNav);
+
+            setTimeout(() => {
+                this.resize();
+            }, 500)
+        });
     },
 
     resize() {
@@ -59,6 +72,17 @@ const InvestSlider = {
         this.navArray.forEach(element => {
             element.resize();
         })
+    },
+
+    addEvent() {
+        this.switcher.forEach(element => {
+            element.addEventListener('click', e => {
+                const $this = e.currentTarget;
+                this.switcher.forEach(element => {element.classList.remove(this.class)});
+                $this.classList.add(this.class);
+                this.wrapper.dataset.investSlider = $this.dataset.investSliderIndex;
+            });
+        });
     },
 };
 
--- resources/views/sections/invest-gallery.blade.php
+++ resources/views/sections/invest-gallery.blade.php
@@ -1,4 +1,14 @@
-<section class="section section--half-top section--shade" id="galle">
+@php
+    $slidersName = ['Wizualizacje', 'Wnętrza', 'Okolica'];
+    $sliders = [];
+    $slider1 = get_field('slider1');
+    $slider2 = get_field('slider2');
+    $slider3 = get_field('slider3');
+
+    array_push($sliders, $slider1, $slider2, $slider3)
+@endphp
+
+<section class="section section--half-top section--no-bottom section--shade" id="gallery">
     <div class="container">
         <header class="section__header">
             <h3 class="section__title title">
@@ -8,30 +18,47 @@
                 <br>
                 Galeria zdjęć
             </h3>
+            @if ($slidersName)
+            <nav class="invest-slider-nav">
+                <ul class="invest-slider-nav__list">
+                    @foreach ($slidersName as $item)
+                    <li class="invest-slider-nav__elem">
+                        <button class="invest-slider-nav__button button button--rev @if($loop->first) -is-active @endif" data-invest-slider-index="{{ $loop->index + 1 }}">
+                            {{ $item }}
+                        </button>
+                    </li>
+                    @endforeach
+                </ul>
+            </nav>
+            @endif
         </header>
     </div>
-    @php
-        $slider1 = get_field('slider1');
-    @endphp
-    @if ($slider1)
-    <div class="invest-slider">
-        <div class="invest-slider__carousel">
-            @foreach ($slider1 as $elem)
-            <div class="invest-slider__cell">
-                {!! image($elem['ID'], 'full', 'invest-slider__image') !!}
-            </div>
-            @endforeach
-        </div>
-        
-        <div class="container">
-            <div class="invest-slider__nav-carousel">
-                @foreach ($slider1 as $elem)
-                <div class="invest-slider__nav-cell">
-                    {!! image($elem['ID'], 'full', 'invest-slider__image--nav') !!}
+    
+    @if($sliders)
+    <div class="invest-slider" data-invest-slider="1">
+        @foreach ($sliders as $slider)
+        @if ($slider)
+        <div class="invest-slider__wrapper">
+            <div class="invest-slider__carousel">
+                @foreach ($slider as $elem)
+                <div class="invest-slider__cell">
+                    {!! image($elem['ID'], 'full', 'invest-slider__image') !!}
                 </div>
                 @endforeach
             </div>
+            
+            <div class="container">
+                <div class="invest-slider__nav-carousel">
+                    @foreach ($slider as $elem)
+                    <div class="invest-slider__nav-cell">
+                        {!! image($elem['ID'], 'full', 'invest-slider__image--nav') !!}
+                    </div>
+                    @endforeach
+                </div>
+            </div>
         </div>
+        @endif
+        @endforeach
     </div>
     @endif
 </section>
