465b72547790d9c83ac8f16bb475a67e2071e350
Kamil Mirosz <kmirosz6@gmail.com>
2020-01-11 08:19:30 +0100

    fix  form

--- resources/assets/scripts/components/form.js
+++ resources/assets/scripts/components/form.js
@@ -23,15 +23,24 @@ const Form = {
 
       form.addClass(CLASS);
 
-      $.ajax({
-        type: 'POST',
-        url: url,
-        data: form.serialize(), // serializes the form's elements.
-        success: function (data) {
-          form.innerHTML = `<p class="form__message">${form.dataset.aftersend}</p>`
-          console.log('Send', data); // show response from the php script.
-        },
-      });
+      console.log('send', url);
+      form.serialize();
+      form.css('min-height', form.height());
+      form.html(`<h3 class="contact__title title">Formularz kontaktowy</h3><p class="contact__message">${form.data('aftersend')}</p>`);
+
+      // $.ajax({
+      //   type: 'POST',
+      //   url: url,
+      //   data: form.serialize(), // serializes the form's elements.
+      //   success: function (data) {
+
+      //     $('.form').html('test');
+      //     console.log('Send', data); // show response from the php script.
+      //   },
+      //   error: function () {
+      //     form.innerHTML = '<p class="form__message">Nie udało się wysłać wiadomości</p>';
+      //   },
+      // });
     })
   },
 }
